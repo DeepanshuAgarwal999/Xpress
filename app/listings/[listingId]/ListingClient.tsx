d4c9667458e9ff92cf3c10b71692f724296d1a2f
rupeshshandilya <rupeshkshandilya@gmail.com>
2024-02-12 13:49:55 +0530

    refactor(paymentverify/route.js): remove unused import dbConnect to clean up code fix(ListingClient.tsx): update API endpoints to correct domain for razorpay and paymentverify to ensure correct server communication

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -138,7 +138,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
             const key = process.env.RAZORPAY_API_KEY;
             console.log(key);
             // Make API call to the serverless API
-            const data = await fetch("https://book.thexpresssalon.com/api/razorpay", {
+            const data = await fetch("https://www.thexpresssalon.com/api/razorpay", {
               method: "POST",
               body: JSON.stringify({
                 totalPriceAfterTaxid: parseInt(totalPriceAfterTax),
@@ -161,7 +161,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
               }) {
                 console.log("HERE" + response);
                 const data = await fetch(
-                  "https://book.thexpresssalon.com/api/paymentverify",
+                  "https://www.thexpresssalon.com/api/paymentverify",
                   {
                     method: "POST",
                     body: JSON.stringify({
